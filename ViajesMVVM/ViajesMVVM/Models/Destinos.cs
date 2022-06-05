8742e75e142cb64d1d13b06fdaaef44eb9f918e7
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2022-06-05 16:14:22 -0500

    Models Cambios

---
+++ ViajesMVVM/ViajesMVVM/Models/Destinos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViajesMVVM.Models
+{
+    public class Destinos
+    {
+        public string ImagenLugar { get; set; } = "";
+        public string LocacionMapa { get; set; } = "";
+        public string NombreLugar { get; set; } = "";
+        public string Ciudad { get; set; } = "";
+        public string Pais { get; set; } = "";
+
+        public string Descripcion { get; set; } = "";
+        public decimal Costo { get; set; }
+        
+    }
+}
